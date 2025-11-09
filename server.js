5d21eb281149395f2f9af72b908c472bc98a52c8
iamraghavan <developer.raghavan@gmail.com>
2025-11-09 10:09:34 +0530

    `Removed cors middleware`

--- server.js
+++ server.js
@@ -29,7 +29,7 @@ app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 // Security & sanitization
 app.use(helmet());
-app.use(cors());
+// app.use(cors());
 app.use(mongoSanitize());
 
 // Logging
