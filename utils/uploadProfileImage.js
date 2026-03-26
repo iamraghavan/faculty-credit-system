b15f8a936d7550a810ffa2bf59924a8aaf0c5194
iamraghavan <developer.raghavan@gmail.com>
2026-03-26 07:51:52 +0530

    01010

--- utils/uploadProfileImage.js
+++ utils/uploadProfileImage.js
@@ -9,11 +9,23 @@ const { uploadFileToGitHub } = require('./githubUpload');
 async function handleProfileImageUpload(file, folder = 'profileImages') {
   if (!file) return null;
 
-  const destPath = `${folder}/${Date.now()}-${Math.round(Math.random() * 1e9)}${file.originalname}`;
+  const originalName = file.originalname || file.name || file.originalFilename || 'image.png';
+  const destPath = `${folder}/${Date.now()}-${Math.round(Math.random() * 1e9)}-${originalName}`;
 
-  // Upload using buffer
-  const url = await uploadFileToGitHub(file.buffer, destPath, file.originalname);
-  return url;
+  // If we have a buffer (Multer), use uploadFileToGitHubBuffer
+  if (file.buffer) {
+    const { uploadFileToGitHubBuffer } = require('./githubUpload');
+    return await uploadFileToGitHubBuffer(file.buffer, destPath);
+  }
+
+  // If we have a path (Formidable), use uploadFileToGitHub
+  const localPath = file.filepath || file.path;
+  if (localPath) {
+    const { uploadFileToGitHub } = require('./githubUpload');
+    return await uploadFileToGitHub(localPath, destPath);
+  }
+
+  throw new Error('Invalid file object: no buffer or path found');
 }
 
 module.exports = { handleProfileImageUpload };
